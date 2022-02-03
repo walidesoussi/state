b69ee892e97d5f679c3daefc54679f712d2a93b2
walidesoussi <eoussi.walid@gmail.com>
2022-02-03 16:58:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import imgsrc from './walid.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Walid SOUSSI",
+      bio: " I am a Senior Civil Engineer And Junior Web Developer .",
+      img:imgsrc,
+    },
+    show: true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <h1 className="fullName">{this.state.Person.fullName}</h1>
+            <h1 className="bio">{this.state.Person.bio}</h1>
+            <img src={this.state.Person.img} alt="walid"></img>          
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
 
 export default App;
